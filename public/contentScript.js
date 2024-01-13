a0e7afdc37bb26b319f39c00fcd39029d2b4a138
darshit <darshit.hs@somaiya.edu>
2024-01-13 18:32:38 +0530

    limited scraping

--- public/contentScript.js
+++ public/contentScript.js
@@ -4,9 +4,9 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
   if (message.action === "scrape") {
     const mainContent = [];
 
-    // Get all h1, h2, h3, p, span, and li elements, excluding those within nav or footer themselves
+    // Get all h1, h2, h3, h4, p, span, and li elements, excluding those within nav, sidebar, or footer
     const elements = document.querySelectorAll(
-      "body > :not(nav):not(footer) h1, body > :not(nav):not(footer) h2, body > :not(nav):not(footer) h3, body > :not(nav):not(footer) p, body > :not(nav):not(footer) span, body > :not(nav):not(footer) li"
+      "body > :not(nav):not(sidebar):not(footer) h1, body > :not(nav):not(sidebar):not(footer) h2, body > :not(nav):not(sidebar):not(footer) h3, body > :not(nav):not(sidebar):not(footer) h4, body > :not(nav):not(sidebar):not(footer) p, body > :not(nav):not(sidebar):not(footer) span, body > :not(nav):not(sidebar):not(footer) li"
     );
 
     // Iterate through the elements and merge h1/h2/h3 with the following p/span, excluding those with less than or equal to 4 words
@@ -14,17 +14,19 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
       const currentElement = elements[i];
       let mergedText = currentElement.innerText;
 
-      if (mergedText.split(" ").length > 4) { // Check for more than 4 words
+      if (mergedText.split(" ").length > 4) {
+        // Check for more than 4 words
         if (
           (currentElement.tagName === "H1" ||
             currentElement.tagName === "H2" ||
-            currentElement.tagName === "H3") &&
+            currentElement.tagName === "H3" ||
+            currentElement.tagName === "H4") &&
           elements[i + 1] &&
           (elements[i + 1].tagName === "P" ||
             elements[i + 1].tagName === "SPAN" ||
             elements[i + 1].tagName === "LI")
         ) {
-          mergedText += " " + elements[i + 1].innerText;
+          mergedText += " -> " + elements[i + 1].innerText;
           i++; // Skip the next element since it's already merged
         }
 
@@ -35,5 +37,3 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
     sendResponse({ data: mainContent });
   }
 });
-
-
