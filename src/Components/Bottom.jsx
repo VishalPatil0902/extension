8c523d7e221b0c6d833de27f4ff2f94d62522736
darshit <darshit.hs@somaiya.edu>
2024-01-17 17:21:42 +0530

    pdf_saving

--- src/Components/Bottom.jsx
+++ src/Components/Bottom.jsx
@@ -1,90 +1,123 @@
-import React, { useState, useEffect ,useRef} from 'react';
-import { Stack, Button, Icon, Text, Tooltip } from '@chakra-ui/react';
-import { FaImage, FaFilePdf } from 'react-icons/fa';
-import axios from 'axios';
+import React, { useState, useEffect, useRef } from "react";
+import { Stack, Button, Icon, Text, Tooltip } from "@chakra-ui/react";
+import { FaImage, FaFilePdf } from "react-icons/fa";
+import axios from "axios";
 
 const Bottom = () => {
-  
   const inputRef = useRef(null);
   const [file, setFile] = useState(null);
-  const [del,setdel]=useState(0);
-  const [uploadProgress,setUploadProgress]=useState(0);
+  const [uploadInProgress, setUploadInProgress] = useState(false);
 
-  const handleFileChange = async (event) => {
-    
+  const handleFileChange = (event) => {
     setFile(event.target.files[0]);
-
   };
 
-  const handleDel = async () => {
-    if (del === 1) {
-      try {
-        await axios.delete(`http://localhost:3000/api/deletefile/${file.name}`);
-        console.log('File deleted successfully!');
-        setFile(null); // Clear the file reference after deletion
-        setdel(0);     // Reset del state to 0 after deletion
-      } catch (error) {
-        console.error('File deletion failed:', error);
-      }
-    } else {
-      console.log('nothing to delete');
+  const handleUpload = async () => {
+    if (!file || uploadInProgress) {
+      console.error("No file selected or upload in progress.");
+      return;
     }
-  };
-  
-  useEffect(() => {
-    async function upload() {
-      if (!file) {
-        console.error('No file selected.');
-        return;
-      }
-  
-      const formData = new FormData();
-      formData.append('myFile', file);
-      console.log(formData);
-  
-      try {
-        const response = await axios.post('http://localhost:3000/api/uploadfile', formData, {
-          headers: {
-            'Content-Type': 'multipart/form-data',
-          },
-        });
-  
-        if (response.status === 200) {
-          console.log('File uploaded successfully!');
-        } else {
-          console.error('File upload failed.');
-        }
-      } catch (error) {
-        console.error('File upload failed:', error);
-      }
-    }
-  
-    if (del === 0 && file !== null) {
-      upload();
-    }
-  
+
+    setUploadInProgress(true);
     console.log(file);
-  }, [file, del]);
-  
+    const formData = new FormData();
+    formData.append("myFile", file);
 
+    try {
+      const response = await axios.post(
+        "http://localhost:3000/api/uploadfile",
+        formData,
+        {
+          headers: {
+            "Content-Type": "multipart/form-data",
+          },
+          onUploadProgress: (progressEvent) => {
+            const progress = Math.round(
+              (progressEvent.loaded * 100) / progressEvent.total
+            );
+            console.log("Upload Progress: " + progress + "%");
+          },
+        }
+      );
+
+      if (response.status === 200) {
+        console.log("File uploaded successfully!");
+      } else {
+        console.error("File upload failed.");
+      }
+    } catch (error) {
+      console.error("File upload failed:", error);
+    } finally {
+      setUploadInProgress(false);
+    }
+  };
+
+  const handleDelete = async () => {
+    try {
+      await axios.delete(`http://localhost:3000/api/deletefile/${file.name}`);
+      console.log("File deleted successfully!");
+      setFile(null); // Clear the file reference after successful deletion
+      inputRef.current.value = "";
+    } catch (error) {
+      console.error("File deletion failed:", error);
+    }
+  };
 
   return (
-    <Stack direction="row" spacing={3} width="100%" marginTop="0.5rem" align="center">
+    <Stack
+      direction="row"
+      spacing={3}
+      width="100%"
+      marginTop="0.5rem"
+      align="center"
+    >
       <Tooltip label="Upload Image" fontSize="md">
-        <Button colorScheme="orange" variant="solid" onClick={handleDel}>
+        <Button
+          colorScheme="orange"
+          variant="solid"
+          onClick={handleUpload}
+          disabled={uploadInProgress}
+        >
           <Icon as={FaImage} />
         </Button>
       </Tooltip>
 
       <Tooltip label="Upload PDF" fontSize="md">
-         <Button colorScheme="blue" variant="solid" onClick={() => inputRef.current.click()}>
-            <Icon as={FaFilePdf} />
-         </Button>
+        <Button
+          colorScheme="blue"
+          variant="solid"
+          onClick={() => inputRef.current.click()}
+          disabled={uploadInProgress}
+        >
+          <Icon as={FaFilePdf} />
+        </Button>
       </Tooltip>
 
-      {file && <Text marginLeft="2" color="white">{file.name}</Text>}
-      <input type="file" onChange={handleFileChange}     accept="application/pdf" style={{display:'none'}} ref={inputRef}/>
-
+      {file && (
+        <>
+          <Text marginLeft="2" color="white">
+            {file.name}
+          </Text>
+          <Tooltip label="Delete File" fontSize="md">
+            <Button
+              colorScheme="red"
+              variant="solid"
+              onClick={handleDelete}
+              disabled={uploadInProgress}
+            >
+              Delete
+            </Button>
+          </Tooltip>
+        </>
+      )}
+      <input
+        type="file"
+        onChange={handleFileChange}
+        accept="application/pdf"
+        style={{ display: "none" }}
+        ref={inputRef}
+        disabled={uploadInProgress}
+      />
     </Stack>
   );
 };
--- src/Components/Search.jsx
+++ src/Components/Search.jsx
@@ -1,39 +1,110 @@
 /* global chrome */
+// import for vector tokenizer
+// import { TogetherAI } from "@langchain/community/llms/togetherai";
+// import { PromptTemplate } from "@langchain/core/prompts";
+// import { FaissStore } from "@langchain/community/vectorstores/faiss";
+// import { HuggingFaceInferenceEmbeddings } from "langchain/embeddings/hf";
+// import { RecursiveCharacterTextSplitter } from "langchain/text_splitter";
+// import { VectorDBQAChain } from "langchain/chains";
 
-import React,{useState} from 'react'
-import { Stack,Button,Icon } from '@chakra-ui/react'
-import { SearchIcon,ChatIcon } from '@chakra-ui/icons'
+// other imports
+import React, { useState } from "react";
+import { Stack, Button, Icon } from "@chakra-ui/react";
+import { SearchIcon, ChatIcon } from "@chakra-ui/icons";
+import { CheerioWebBaseLoader } from "langchain/document_loaders/web/cheerio";
 
-const Search = ({onNewChat,Extracte}) => {
-  
-  const [code, setCode] = useState("");
-
+const Search = ({ onNewChat, Extracte }) => {
   // Example in your sidebar React component
+  /* global chrome */
+
+  const [code, setCode] = useState("");
+  const handleGetHTML = async () => {
+    const [tab] = await chrome.tabs.query({
+      active: true,
+      currentWindow: true,
+    });
+    const [result] = await chrome.scripting.executeScript({
+      target: { tabId: tab.id },
+      func: () => document.documentElement.outerHTML,
+    });
+    console.log(result); // Do something with the HTML
+    setCode(result);
+  };
+  // Example in your sidebar React component
+
   const handleScrape = () => {
     chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
       const activeTabId = tabs[0].id;
-      console.log(activeTabId);
+      const activeTabUrl = tabs[0].url; // Extract the URL of the active tab
 
-      // Send a message to the content script to initiate scraping
-      chrome.tabs.sendMessage(activeTabId, { action: "scrape" }, (response) => {
-        console.log(JSON.stringify(response));
-        setCode(response);
-      });
+      fetchData(activeTabUrl);
     });
-  };
 
-  
-  
+    async function fetchData(url) {
+      try {
+        // Creating an instance of CheerioWebBaseLoader
+        const loader = new CheerioWebBaseLoader(url, {
+          selector:
+            "[class^=content], [class*= content], [class$=content] div, [class^=content], [class*= content], [class$=content] article, [class^=content], [class*= content], [class$=content] p",
+        });
+
+        // Loading the document asynchronously
+        const docs = await loader.load();
+
+        // Replace multiple white spaces with a single space
+        const cleanedContent = docs[0].pageContent.replace(/\s+/g, " ").trim();
+
+        setCode(cleanedContent);
+        console.log(cleanedContent);
+        // vectorTokenize(cleanedContent);
+      } catch (error) {
+        console.error("Error:", error);
+      }
+    }
+  };
+  // const vectorTokenize = async (textData) => {
+  //   const textSplitter = new RecursiveCharacterTextSplitter({
+  //     chunkSize: 1000,
+  //     chunkOverlap: 100,
+  //   });
+
+  //   const docs = await textSplitter.createDocuments([textData]);
+  //   const texts = await textSplitter.splitText(textData);
+
+  //   const embeddings = new HuggingFaceInferenceEmbeddings({
+  //     model: "sentence-transformers/all-MiniLM-L6-v2",
+  //   });
+
+  //   const ids = createArrayWithIds(texts.length);
+
+  //   const vectorStore = await FaissStore.fromTexts(texts, ids, embeddings);
+
+  //   await vectorStore.save("../Assets/vectorstore");
+  // };
+
   return (
-    <Stack direction='row' spacing={3} width='100%' marginTop='0.5rem'>
-      <Button leftIcon={<ChatIcon />} bg={'#6fa6cb'} color={'#fff'} variant='solid' width='100%' onClick={onNewChat}>
+    <Stack direction="row" spacing={3} width="100%" marginTop="0.5rem">
+      <Button
+        leftIcon={<ChatIcon />}
+        bg={"#6fa6cb"}
+        color={"#fff"}
+        variant="solid"
+        width="100%"
+        onClick={onNewChat}
+      >
         New Chat
       </Button>
-      <Button rightIcon={<SearchIcon />} colorScheme='blue' variant='solid' width='100%' onClick={handleScrape}>
+      <Button
+        rightIcon={<SearchIcon />}
+        colorScheme="blue"
+        variant="solid"
+        width="100%"
+        onClick={handleScrape}
+      >
         Read Website
       </Button>
     </Stack>
-  )
-}
+  );
+};
 
-export default Search
+export default Search;
