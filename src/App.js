29f8b5c55e02e42dceeaeebe3ecf7c78735d10df
Vishal Patil <vishalpatil6924@gmail.com>
2024-01-17 11:14:26 +0530

    Tabs

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+/* global chrome */
 import "./App.css";
 import SidePanelToggle from "./SidePanelToggle";
 import ActiveTabURL from "./pages/ActiveTab/ActiveURL";
@@ -6,7 +7,7 @@ import Extraction from "./pages/scripting/Extraction";
 import { ChakraProvider } from '@chakra-ui/react'
 import Home from "./pages/Home/Home";
 import Search from "./Components/Search";
-
+import Bottom from "./Components/Bottom";
 
 function App() {
   let [getURL, setURL] = useState(0);
@@ -26,15 +27,15 @@ function App() {
       },
     ]);
   };
+
+
   
   return (
     <ChakraProvider>
         <div className="App">
-            {/* <ActiveTabURL getURL={getURL} />
-            <SidePanelToggle getURL={getURL} setURL={setURL} />
-            <Extraction/> */}
             <Search onNewChat={handleNewChat} />
             <Home messages={messages} setMessages={setMessages} />
+            <Bottom/>
         </div>
     </ChakraProvider>
   );    
--- src/Components/Search.jsx
+++ src/Components/Search.jsx
@@ -1,14 +1,35 @@
-import React from 'react'
+/* global chrome */
+
+import React,{useState} from 'react'
 import { Stack,Button,Icon } from '@chakra-ui/react'
 import { SearchIcon,ChatIcon } from '@chakra-ui/icons'
 
-const Search = ({onNewChat}) => {
+const Search = ({onNewChat,Extracte}) => {
+  
+  const [code, setCode] = useState("");
+
+  // Example in your sidebar React component
+  const handleScrape = () => {
+    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+      const activeTabId = tabs[0].id;
+      console.log(activeTabId);
+
+      // Send a message to the content script to initiate scraping
+      chrome.tabs.sendMessage(activeTabId, { action: "scrape" }, (response) => {
+        console.log(JSON.stringify(response));
+        setCode(response);
+      });
+    });
+  };
+
+  
+  
   return (
     <Stack direction='row' spacing={3} width='100%' marginTop='0.5rem'>
       <Button leftIcon={<ChatIcon />} bg={'#6fa6cb'} color={'#fff'} variant='solid' width='100%' onClick={onNewChat}>
         New Chat
       </Button>
-      <Button rightIcon={<SearchIcon />} colorScheme='blue' variant='solid' width='100%'>
+      <Button rightIcon={<SearchIcon />} colorScheme='blue' variant='solid' width='100%' onClick={handleScrape}>
         Read Website
       </Button>
     </Stack>
