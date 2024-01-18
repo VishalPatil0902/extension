f06932cccf2dad0bf83961303c719eda82ebefe3
Pranav Patil <pranav07@somaiya.edu>
2024-01-18 21:23:01 +0530

    package.json updates and solved dependencies

--- src/pages/scripting/Extraction.jsx
+++ src/pages/scripting/Extraction.jsx
@@ -1,6 +1,11 @@
 /* global chrome */
 
 import React, { useState } from "react";
+import { TogetherAI } from "@langchain/community/llms/togetherai";
+import { MemoryVectorStore } from "langchain/vectorstores/memory";
+import { HuggingFaceInferenceEmbeddings } from "langchain/embeddings/hf";
+import { RecursiveCharacterTextSplitter } from 'langchain/text_splitter';
+import {ConversationalRetrievalQAChain} from 'langchain/chains';
 
 export default function Extraction() {
   const [code, setCode] = useState("");
@@ -16,7 +21,6 @@ export default function Extraction() {
     console.log(result); // Do something with the HTML
     setCode(result);
   };
-  // Example in your sidebar React component
 
   const handleScrape = () => {
     chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
